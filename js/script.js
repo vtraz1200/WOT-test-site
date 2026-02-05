5d234eb5f32abf39d74ba66dee64ccdb1c7d2d31
Vontraz Glasper <mason7379@gmail.com>
2026-02-04 21:00:48 -0500

    second commit

--- js/script.js
+++ js/script.js
@@ -88,6 +88,27 @@ if (sectionHeroThirdEl) {
   obs.observe(sectionHeroThirdEl);
 }
 
+const sectionHeroFourthEl = document.querySelector(".section-hero-details");
+if (sectionHeroFourthEl) {
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+
+      if (!ent.isIntersecting) {
+        document.body.classList.add("sticky");
+      } else {
+        document.body.classList.remove("sticky");
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: "-80px",
+    },
+  );
+  obs.observe(sectionHeroFourthEl);
+}
+
 //////////////////////////////////////////////////////////
 // REVEAL SECTIONS
 
